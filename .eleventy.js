ab19c5d2e976ba2e3d65e990f719ce3432cbc609
t.k.langston <mail@designbytk.com>
2025-12-01 16:18:37 -0500

    Upgrade to 11ty 3.0 Fixes #7

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
-const esbuild = require("esbuild")
+import esbuild from "esbuild";
+import pugPlugin from "@11ty/eleventy-plugin-pug";
+import { sassPlugin } from "esbuild-sass-plugin";
 
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
 
 	// process bundling for javascript files
 	// use esbuild
@@ -12,6 +14,21 @@ module.exports = function (eleventyConfig) {
       outdir: 'dist/js/'
     	}
     )
+		await esbuild.build({
+      entryPoints: ['src/sass/main.sass'],
+      sourcemap: false,
+			minify: true,
+			outfile: 'dist/css/main.min.css',
+			plugins: [
+         sassPlugin({
+						// Rewriting relative url(...)s https://github.com/glromeo/esbuild-sass-plugin#--rewriting-relative-urls
+						// precompile(source, pathname) {
+						// 	const basedir = path.dirname(pathname)
+						// 	return source.replace(/(url\(['"]?)(\.\.?\/)([^'")]+['"]?\))/g, `$1${basedir}/$2$3`)
+						// }
+				 })
+			]
+		})
   })
 	
 	// Copy `src/static/` to `dist/`
@@ -21,7 +38,8 @@ module.exports = function (eleventyConfig) {
 
 	// pug settings - add eleventy filters to pug
 	global.filters = eleventyConfig.javascriptFunctions;
-	
+
+	eleventyConfig.addPlugin(pugPlugin);
   eleventyConfig.setPugOptions({
     globals: ['filters'],
     pretty: true
